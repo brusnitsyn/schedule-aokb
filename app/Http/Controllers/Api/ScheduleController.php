9e2b2fdc03bd25ec3987fb4a5e023f0394a9fa1e
brusnitsyn <andrew.brusnitsyn@gmail.com>
2024-12-13 16:25:17 +0900

    update chunked data

--- app/Http/Controllers/Api/ScheduleController.php
+++ app/Http/Controllers/Api/ScheduleController.php
@@ -11,13 +11,13 @@ class ScheduleController extends Controller
     {
         $items = \App\Models\ScheduleItem::orderBy('room')->with('statusScheduleItem')->get();
         $chunks = collect();
-        if (count($items) <= 10) {
+        if (count($items) <= 12) {
             $chunks->push($items);
         } else {
             $tempArray = collect();
             $lastItem = $items->last();
             foreach ($items as $item) {
-                if (count($tempArray) === 10) {
+                if (count($tempArray) === 12) {
                     $chunks->push($tempArray);
                     $tempArray = collect();
                 }
