09a074c02f5a7526d893817e1e41509b422c37fc
Andrew Brusnitsyn <42877650+brusnitsyn@users.noreply.github.com>
2025-05-06 08:08:13 +0900

    Fix Datetime field overflow

--- app/Http/Requests/Admin/Schedule/ScheduleStoreRequest.php
+++ app/Http/Requests/Admin/Schedule/ScheduleStoreRequest.php
@@ -36,7 +36,14 @@ class ScheduleStoreRequest extends FormRequest
 
     public function store()
     {
-        $hasCreated = ScheduleItem::create($this->validated());
+        $data = $this->validated();
+
+        Log::info($scheduleItem);
+
+        if (intval($data['start_at'])) $data['start_at'] = Carbon::createFromTimestampMs($data['start_at'], config('app.timezone'))->toDateTime();
+        if (intval($data['end_at'])) $data['end_at'] = Carbon::createFromTimestampMs($data['end_at'], config('app.timezone'))->toDateTime();
+        
+        $hasCreated = ScheduleItem::create($data);
         if ($hasCreated) {
             broadcast(new CreatedScheduleItem($hasCreated->load('statusScheduleItem')));
         }
