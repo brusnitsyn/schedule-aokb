325a06c288bac517a899ebbfae03fef8b84cc2b3
brusnitsyn <brusnitsyn@gmail.com>
2025-05-22 08:07:38 +0900

    Изменение в роутинге

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,9 @@ use Illuminate\Support\Facades\Route;
 //    'schedule' => \App\Models\ScheduleItem::orderBy('id')->with('statusScheduleItem')->get()
 //]);
 
-Route::get('/', [\App\Http\Controllers\ScheduleController::class, 'page'])->name('schedule.page');
+//Route::get('/', [\App\Http\Controllers\ScheduleController::class, 'page'])->name('schedule.page');
 
-Route::get('/schedule', [\App\Http\Controllers\ScheduleController::class, 'index'])->name('schedule');
+Route::get('/', [\App\Http\Controllers\ScheduleController::class, 'index'])->name('schedule');
 
 Route::post('/schedule/create', [\App\Http\Controllers\ScheduleController::class, 'create'])->name('schedule.create');
 Route::post('/schedule/{scheduleItem}/update', [\App\Http\Controllers\ScheduleController::class, 'update'])->name('schedule.update');
